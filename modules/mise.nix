87d523a627702d6cae10d6e7b102313a4632c3c4
joaopfusco <joaopedrofusco@gmail.com>
2026-02-26 08:59:44 -0300

    refactor: Remove global configuration settings from mise module

--- modules/mise.nix
+++ modules/mise.nix
@@ -7,22 +7,6 @@
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
-    
-    globalConfig = {
-      settings = {
-        experimental = true;
-        always_keep_download = false;
-        plugin_autoupdate_last_check_duration = "1 week";
-      };
-      
-      tools = {
-        node = "lts";
-        python = "3.12";
-        go = "1.25.6";
-        dotnet = "latest";
-        rust = "stable";
-      };
-    };
   };
 
   home.sessionPath = [
