b7d166fb6cc64a43404d328dfae48856aa7af599
joaopfusco <joaopedrofusco@gmail.com>
2026-02-28 11:59:59 -0300

    fix: add SSH configuration and enable ssh-agent for improved security

--- modules/git.nix
+++ modules/git.nix
@@ -9,5 +9,24 @@
         email = "joaopedrofusco@gmail.com";
       };
     };
+    credential = {
+      helper = "store";
+    };
   };
+
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+
+    matchBlocks = {
+      "github.com gitlab.com" = {
+        identityFile = "~/.ssh/id_ed25519";
+        user = "git";
+        addKeysToAgent = "yes";
+        forwardAgent = false;
+      };
+    };
+  };
+
+  services.ssh-agent.enable = true;
 }
