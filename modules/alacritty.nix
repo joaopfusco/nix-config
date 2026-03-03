3e991f86272210a7fbf29d9030311cd0834dfd79
joaopfusco <joaopedrofusco@gmail.com>
2026-03-03 20:05:18 -0300

    feat: Add Alacritty configuration and configure Zsh as the default interactive shell.

---
+++ modules/alacritty.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        dimensions = { columns = 120; lines = 35; };
+        padding = { x = 10; y = 10; };
+      };
+    };
+  };
+}
