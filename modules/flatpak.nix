194b4f93ed6e970664cf176d71cc98216849b657
joaopfusco <joaopedrofusco@gmail.com>
2026-03-06 16:04:09 -0300

    feat: Update Flatpak package list by adding Flatseal, DistroShelf, and virt-manager

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -8,13 +8,16 @@
     }];
 
     packages = [
+      "com.github.tchx84.Flatseal"
       "com.discordapp.Discord"
       "io.dbeaver.DBeaverCommunity"
       "org.libreoffice.LibreOffice"
       "com.obsproject.Studio"
       "us.zoom.Zoom"
-      "page.codeberg.libre_menu_editor.LibreMenuEditor" 
+      "page.codeberg.libre_menu_editor.LibreMenuEditor"
       "org.videolan.VLC"
+      "com.ranfdev.DistroShelf"
+      "org.virt_manager.virt-manager"
     ];
 
     update.auto.enable = true;
