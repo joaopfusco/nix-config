a67e458dc3245649f0ca5f5b252e19516805c3ba
joaopfusco <joaopedrofusco@gmail.com>
2026-03-07 18:44:49 -0300

    feat: Remove nix-flatpak module from flake configuration

--- flake.nix
+++ flake.nix
@@ -7,10 +7,9 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-flatpak, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -20,10 +19,7 @@
     in {
       homeConfigurations."joaop" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [
-          nix-flatpak.homeManagerModules.nix-flatpak
-          ./home.nix 
-        ];
+        modules = [ ./home.nix ];
       };
     };
 }
