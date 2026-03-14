7d51609d5baa5127f8fcb1836f3919ab7d8d62f9
joaopfusco <joaopedrofusco@gmail.com>
2026-03-14 13:40:19 -0300

    refactor: Remove inline Flatpak service configuration in home.nix, use dedicated flatpak.nix module

--- home.nix
+++ home.nix
@@ -20,30 +20,6 @@
     fastfetch
   ];
 
-  services.myflatpak = {
-    enable = true;
-    uninstallUnmanaged = true;
-
-    remotes = [{
-      name = "flathub";
-      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-    }];
-
-    packages = [
-      "com.github.tchx84.Flatseal"
-      "org.videolan.VLC"
-      "com.discordapp.Discord"
-      "io.dbeaver.DBeaverCommunity"
-      "org.libreoffice.LibreOffice"
-      "com.obsproject.Studio"
-      "page.codeberg.libre_menu_editor.LibreMenuEditor"
-      "com.ranfdev.DistroShelf"
-      "us.zoom.Zoom"
-    ];
-
-    update.auto.enable = true;
-  };
-
   home.sessionVariables = {
     NIX_PATH = "nixpkgs=${pkgs.path}";
   };
--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,81 +1,31 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
-with lib;
+{
+  imports = [
+    ../services/myflatpak.nix
+  ];
 
-let
-  cfg = config.services.myflatpak;
-  flatpakBin = "/usr/bin/flatpak";
+  services.myflatpak = {
+    enable = true;
+    uninstallUnmanaged = true;
 
-  remoteCmds = concatStringsSep "\n" (map (remote: 
-    "${flatpakBin} remote-add --user --if-not-exists ${remote.name} ${remote.location}"
-  ) cfg.remotes);
+    remotes = [{
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }];
 
-  appList = concatStringsSep " " cfg.packages;
+    packages = [
+      "com.github.tchx84.Flatseal"
+      "org.videolan.VLC"
+      "com.discordapp.Discord"
+      "io.dbeaver.DBeaverCommunity"
+      "org.libreoffice.LibreOffice"
+      "com.obsproject.Studio"
+      "page.codeberg.libre_menu_editor.LibreMenuEditor"
+      "com.ranfdev.DistroShelf"
+      "us.zoom.Zoom"
+    ];
 
-in {
-  options.services.myflatpak = {
-    enable = mkEnableOption "Declarative management of Flatpak";
-    
-    remotes = mkOption {
-      type = types.listOf (types.submodule {
-        options = {
-          name = mkOption { type = types.str; };
-          location = mkOption { type = types.str; };
-        };
-      });
-      default = [];
-    };
-
-    packages = mkOption {
-      type = types.listOf types.str;
-      default = [];
-    };
-
-    update.auto.enable = mkOption {
-      type = types.bool;
-      default = true;
-      description = "If true, runs flatpak update during the switch.";
-    };
-    
-    uninstallUnmanaged = mkOption {
-      type = types.bool;
-      default = false;
-      description = "If true, removes installed apps that are not in the list.";
-    };
-  };
-
-  config = mkIf cfg.enable {
-    home.activation.manageFlatpaks = hm.dag.entryAfter ["writeBoundary"] ''
-      export PATH=$PATH:/usr/bin:/bin
-      
-      # 1. Remotes configration
-      ${remoteCmds}
-
-      # 2. Install/Sync Packages
-      if [ -n "${appList}" ]; then
-        echo "Installing/syncing packages from the list..."
-        ${flatpakBin} install --user -y flathub ${appList}
-      fi
-
-      # 3. Declarative uninstallation (Purge)
-      ${optionalString cfg.uninstallUnmanaged ''
-        echo "Removing unmanaged packages..."
-        installed_apps=$(${flatpakBin} list --user --app --columns=application)
-        for app in $installed_apps; do
-          if [[ ! " ${appList} " =~ " $app " ]]; then
-            ${flatpakBin} uninstall --user -y "$app"
-          fi
-        done
-      ''}
-
-      # 4. Automatic Update
-      ${optionalString cfg.update.auto.enable ''
-        echo "Updating installed packages..."
-        ${flatpakBin} update --user -y
-      ''}
-
-      # 5. Cleanup
-      ${flatpakBin} uninstall --user --unused -y
-    '';
+    update.auto.enable = true;
   };
 }
