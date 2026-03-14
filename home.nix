41f29ad13299080c60d5d625dc063ab53403b291
joaopfusco <joaopedrofusco@gmail.com>
2026-03-14 13:03:13 -0300

    feat: Add Flatpak service configuration with remotes and package list

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     # ./modules/git.nix
+    ./modules/flatpak.nix
     ./modules/zsh.nix
     ./modules/starship.nix
     ./modules/direnv.nix
@@ -18,6 +19,30 @@
     home-manager
     fastfetch
   ];
+
+  services.myflatpak = {
+    enable = true;
+    uninstallUnmanaged = true;
+
+    remotes = [{
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }];
+
+    packages = [
+      "com.github.tchx84.Flatseal"
+      "org.videolan.VLC"
+      "com.discordapp.Discord"
+      "io.dbeaver.DBeaverCommunity"
+      "org.libreoffice.LibreOffice"
+      "com.obsproject.Studio"
+      "page.codeberg.libre_menu_editor.LibreMenuEditor"
+      "com.ranfdev.DistroShelf"
+      "us.zoom.Zoom"
+    ];
+
+    update.auto.enable = true;
+  };
 
   home.sessionVariables = {
     NIX_PATH = "nixpkgs=${pkgs.path}";
