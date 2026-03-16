31672ceafa2937115ac4817f1b82234b412a6f53
joaopfusco <joaopedrofusco@gmail.com>
2026-03-16 15:17:18 -0300

    refactor: Update comments for clarity and consistency in dev.nix

--- modules/dev.nix
+++ modules/dev.nix
@@ -7,23 +7,24 @@
     nodejs_24 # v24
     go_1_25 # v1.25.7
 
-    # Rust
+    # Rust - Obs.: Melhor instlar via script do site oficial
     # rustup
 
-    # .NET SDKs e Runtimes
+    # .NET SDKs e Runtimes - Obs.: Melhor instalar nativamente
     # Obs.: Requires a global.json file in the project to specify the SDK version
     # dotnet new globaljson --sdk-version 8.0.xxx
-    (with dotnetCorePackages; combinePackages [ 
-      sdk_8_0
-      sdk_9_0
-    ])
-    dotnet-ef # or dotnet tool install --global dotnet-ef
+    # (with dotnetCorePackages; combinePackages [ 
+    #   sdk_8_0
+    #   sdk_9_0
+    # ])
+    # dotnet-ef # or dotnet tool install --global dotnet-ef
 
     # Ferramentas
     uv
   ];
 
-  home.sessionPath = [
-    "$HOME/.dotnet/tools"
-  ];
+  # Configurações de ambiente para .NET SDKs e Runtimes
+  # home.sessionPath = [
+  #   "$HOME/.dotnet/tools"
+  # ];
 }
