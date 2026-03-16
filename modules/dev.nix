73c21d6e0f697d8dc0eeba9cce13c4b189e44ef9
joaopfusco <joaopedrofusco@gmail.com>
2026-03-16 16:58:12 -0300

    refactor: Remove .NET SDKs and Runtimes comments from dev.nix for clarity

--- modules/dev.nix
+++ modules/dev.nix
@@ -10,15 +10,6 @@
     # Rust - Obs.: Melhor instlar via script do site oficial
     # rustup
 
-    # .NET SDKs e Runtimes - Obs.: Melhor instalar nativamente
-    # Obs.: Requires a global.json file in the project to specify the SDK version
-    # dotnet new globaljson --sdk-version 8.0.xxx
-    # dotnet tool install --global dotnet-ef
-    # (with dotnetCorePackages; combinePackages [ 
-    #   sdk_8_0
-    #   sdk_9_0
-    # ])
-
     # Ferramentas
     uv
   ];
@@ -26,6 +17,5 @@
   # Ou home.sessionPath ou ~/.profile
   # home.sessionPath = [
   #   "$HOME/.cargo/bin"
-  #   "$HOME/.dotnet/tools"
   # ];
 }
