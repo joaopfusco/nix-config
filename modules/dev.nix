6a125046c177fc7a66bf2206dbbc9ed9ccd3f524
joaopfusco <joaopedrofusco@gmail.com>
2026-03-16 16:16:08 -0300

    refactor: Improve comments for .NET SDK installation and sessionPath usage in dev.nix

--- modules/dev.nix
+++ modules/dev.nix
@@ -13,18 +13,19 @@
     # .NET SDKs e Runtimes - Obs.: Melhor instalar nativamente
     # Obs.: Requires a global.json file in the project to specify the SDK version
     # dotnet new globaljson --sdk-version 8.0.xxx
+    # dotnet tool install --global dotnet-ef
     # (with dotnetCorePackages; combinePackages [ 
     #   sdk_8_0
     #   sdk_9_0
     # ])
-    # dotnet-ef # or dotnet tool install --global dotnet-ef
 
     # Ferramentas
     uv
   ];
 
+  # Melhor não usar o sessionPath
   # home.sessionPath = [
-  #   "$HOME/.cargo/bin" # ou echo '. "$HOME/.cargo/env"' >> ~/.profile
-  #   "$HOME/.dotnet/tools" # ou echo 'export PATH="$HOME/.dotnet/tools:$PATH"' >> ~/.profile
+  #   "$HOME/.cargo/bin"
+  #   "$HOME/.dotnet/tools"
   # ];
 }
