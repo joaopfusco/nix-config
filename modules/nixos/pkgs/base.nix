f1965425ab12ec7bcefd00a44b78bfba0aeec7e6
joaopfusco <joaopedrofusco@gmail.com>
2026-05-03 20:16:19 -0300

    fix: adding zsh to pkgs/base.nix

--- modules/nixos/pkgs/base.nix
+++ modules/nixos/pkgs/base.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 
 {
+  # Install Zsh
+  programs.zsh.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
