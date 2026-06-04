61b2a99449c9a5ead34b1efcb2a957b2f3d0e607
joaopfusco <joaopedrofusco@gmail.com>
2026-06-04 16:01:16 -0300

    chore: updating flake.lock

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
