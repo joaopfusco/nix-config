00f784b6c89968adb19714be58b50ebc8f160822
joaopfusco <joaopedrofusco@gmail.com>
2026-08-09 00:28:27 -0300

    feat(flake): add host.* namespace and home-manager wiring

---
+++ modules/nix/home-manager.nix
@@ -0,0 +1,43 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.base =
+    { config, ... }:
+    {
+      imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+        extraSpecialArgs = { inherit inputs; };
+      };
+    };
+
+  flake.modules.homeManager.base =
+    { config, lib, pkgs, osConfig, ... }:
+    {
+      config = lib.mkMerge [
+        {
+          nix.registry.pkgs.flake = inputs.self;
+          news.display = "silent";
+          home = {
+            username = config.host.user.name;
+            homeDirectory =
+              if pkgs.stdenv.hostPlatform.isLinux then
+                "/home/${config.host.user.name}"
+              else
+                "/Users/${config.host.user.name}";
+            stateVersion = config.host.state.version;
+            sessionVariables.NIX_PATH = "nixpkgs=${pkgs.path}";
+          };
+        }
+        (lib.mkIf (osConfig == null) {
+          targets.genericLinux.enable = pkgs.stdenv.hostPlatform.isLinux;
+          nix.gc = {
+            automatic = true;
+            dates = "weekly";
+            options = "--delete-older-than 7d";
+          };
+        })
+      ];
+    };
+}
