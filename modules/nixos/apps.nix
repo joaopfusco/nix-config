703fb12706381aef32fef4d3a2ec116aacab3497
joaopfusco <joaopedrofusco@gmail.com>
2026-08-23 15:05:30 -0300

    refactor(nixos): drop gstreamer plugins, disable bluetooth power on boot
    Adjust hardware.graphics options and remove unused media codec packages.

--- modules/nixos/apps.nix
+++ modules/nixos/apps.nix
@@ -4,15 +4,6 @@
     {
       environment.systemPackages =
         (with pkgs; [
-          # media codecs
-          gst_all_1.gstreamer
-          gst_all_1.gst-plugins-base
-          gst_all_1.gst-plugins-good
-          gst_all_1.gst-plugins-bad
-          gst_all_1.gst-plugins-ugly
-          gst_all_1.gst-libav
-
-          # apps
           libreoffice
           vlc
           obs-studio
--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -4,9 +4,9 @@
     hardware.enableAllFirmware = true;
     hardware.enableRedistributableFirmware = true;
     hardware.bluetooth.enable = true;
-    hardware.bluetooth.powerOnBoot = true;
+    hardware.bluetooth.powerOnBoot = false;
+    hardware.graphics.enable = true;
     hardware.graphics.enable32Bit = true;
-
     # services
     services.fwupd.enable = true;
     services.libinput.enable = true;
