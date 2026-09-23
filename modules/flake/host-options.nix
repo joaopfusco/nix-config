0b16a6304400bf6ef8f3fab65305496a0b682ab1
joaopfusco <joaopedrofusco@gmail.com>
2026-09-22 20:22:21 -0400

    style(flake): format host shell enum multiline

--- modules/flake/host-options.nix
+++ modules/flake/host-options.nix
@@ -9,7 +9,10 @@ let
           type = lib.types.str;
         };
         shell = lib.mkOption {
-          type = lib.types.enum [ "zsh" "fish" ];
+          type = lib.types.enum [
+            "zsh"
+            "fish"
+          ];
           default = "zsh";
         };
         user.name = lib.mkOption {
