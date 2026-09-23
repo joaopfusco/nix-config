419e5ac387317320563a4945f3e985dbb33ccbff
joaopfusco <joaopedrofusco@gmail.com>
2026-09-22 22:25:05 -0400

    fix(zsh): source nix-daemon.sh independently of /etc/zshrc

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -11,6 +11,10 @@
         syntaxHighlighting.enable = true;
 
         initContent = ''
+          if [[ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
+            . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+          fi
+
           export PATH="$HOME/.local/bin:$PATH"
           export PATH=/Users/joaop/.opencode/bin:$PATH
 
